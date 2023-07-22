4b4c67d332e3441c5546702d4aa46684c638a35e
Karthik Shankar <misterkar6@gmail.com>
2023-07-21 17:02:10 -0700

    spacing, play button changes, navbar changes

--- app/components/LibraryItem.tsx
+++ app/components/LibraryItem.tsx
@@ -2,18 +2,17 @@ import { Music } from 'lucide-react';
 import Image from 'next/image';
 import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 
-
 // sample Library to use in developing the frontend
 export default function LibraryItem({
-    image,
-    name,
-    creator,
-    type
+  image,
+  name,
+  creator,
+  type,
 }: {
-    image: { src: string; alt: string };
-    name: string;
-    creator: string;
-    type: 'Album' | 'Playlist'
+  image: { src: string; alt: string };
+  name: string;
+  creator: string;
+  type: 'Album' | 'Playlist';
 }) {
   return (
     <div className='relative w-full h-full grid grid-cols-2 rounded-lg hover:bg-gray-400/25'>
@@ -21,15 +20,15 @@ export default function LibraryItem({
         {/** render a square image with the AspectRatio element */}
         <AspectRatio.Root ratio={1}>
           <Image
-            src={image.src}  
+            src={image.src}
             alt={image.alt}
             fill={true}
             sizes='(min-width: 68) 100vw'
             className='rounded-lg'
-            />
+          />
         </AspectRatio.Root>
       </div>
-      <div className='text-[10px] lg:text-sm grid grid-rows-2 flex justify-center items-center ml-2 mr-2'>
+      <div className='text-[10px] lg:text-sm grid grid-rows-2 flex justify-center items-center ml-[10%] mr-[10%]'>
         <div>
           <h1>{name}</h1>
         </div>
@@ -37,13 +36,18 @@ export default function LibraryItem({
           <div className='flex justify-content items-center text-gray-400 opactity-50'>
             <p className='text-[6px] md:text-[8px] lg:text-[10px]'>{type}</p>
             <Music
-              size={32}
+              size={24}
               strokeWidth={2}
               className='ml-0 lg:ml-[2px]'
             />
           </div>
           {/** or other options */}
-          <h2 className='text-[8px] sm:text-[10px] lg:text-sm ml-0 md:ml-[2px] lg:ml-[5px] decoration-solid hover:underline'>{creator}</h2>
+          <div className='flex justify-center items-center'>
+            <h2 className='text-[8px] sm:text-[10px] lg:text-sm ml-0 md:ml-[2px] lg:ml-[5px] decoration-solid hover:underline'>
+              {creator}
+            </h2>
+          </div>
+
           {/** make this a link to artist or user or something maybe? */}
         </div>
       </div>
--- app/components/PlaylistCard.tsx
+++ app/components/PlaylistCard.tsx
@@ -11,32 +11,36 @@ export default function PlaylistCard({
   title: string;
   description: string;
 }) {
-
   return (
     <div className='relative inline-block w-[15%] rounded-xl bg-gray-700/25 ease-in-out duration-300 group/play hover:bg-gray-500/50 mr-12'>
-      <div className='opacity-0 duration-300 ease-in-out group-hover/play:opacity-100'>
+      <div className='opacity-0 absolute top-[45%] left-[60%] brightness-110 w-[40%] h-[40%] duration-300 ease-in-out group-hover/play:opacity-100 group-hover/play:-translate-y-4'>
         <PlayButton />
       </div>
 
       <div className='grid-rows-3 rounded-xl   '>
         <div className='flex justify-center items-center'>
-          <Image
-            src={image.src}
-            alt={image.alt}
-            width={150}
-            height={150}
-            className='rounded-xl mt-2 '
-            priority={true}
-          />
+          <div className='w-[90%] h-[90%]'>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              width={500}
+              height={500}
+              className='rounded-xl mt-[5%] '
+              priority={true}
+            />
+          </div>
         </div>
 
         <div className='text-sm'>
-          <h2 className='text-gray-800 font-semibold ml-2 pt-4 mr-2 truncate'>
+          <h2 className='text-gray-800 font-semibold ml-[10%] pt-4 mr-[10%] truncate'>
             {title}
           </h2>
         </div>
 
-        <DescriptionText title={title} description={description} />
+        <DescriptionText
+          title={title}
+          description={description}
+        />
       </div>
     </div>
   );
