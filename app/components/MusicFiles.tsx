2812866b548ce463cc257a6a336166c8d1563fb2
Karthik Shankar <misterkar6@gmail.com>
2023-07-23 16:38:09 -0700

    music files created from queue

--- app/components/MusicFiles.tsx
+++ app/components/MusicFiles.tsx
@@ -3,24 +3,37 @@ import { usePlayer } from '../context/PlayerContext';
 
 export default function MusicFiles() {
   const { queue } = usePlayer();
+
+  const filesExist = queue.length > 0;
+
+  const files: string[] = [];
+
+  if (filesExist) {
+    queue.forEach((song) => {
+      if (!song) throw new Error('song is null');
+      files.push(song.preview);
+    });
+  }
+
   return (
-    <div>
-      {queue.length > 0 ? (
+    <>
+      {files.length > 0 ? (
         <>
-          {queue.map((item: string, index) => (
+          {files.map((item: string, index) => (
             // autoplay the first song
             <audio
               key={index}
-              controls={true}
               autoPlay={index === 0}
               src={item}
-              id='music'
+              className='audio-file'
+              id={`audio-file-${index}`}
             >
               This browser does not support this audio file.
             </audio>
+            // TODO: how to delete the audio tags when not needed?
           ))}
         </>
       ) : null}
-    </div>
+    </>
   );
 }
