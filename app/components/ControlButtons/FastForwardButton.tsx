4e86445e2288c3d91e1f4487c90475b480b1240a
Karthik Shankar <misterkar6@gmail.com>
2023-07-25 16:05:48 -0700

    refactor, changes in using usePlayer() in Controls

--- app/components/ControlButtons/FastForwardButton.tsx
+++ app/components/ControlButtons/FastForwardButton.tsx
@@ -1,18 +1,27 @@
+import { usePlayer } from '@/app/context/PlayerContext';
 import { FastForward } from 'lucide-react';
 import React, { Dispatch, SetStateAction } from 'react';
 
 export default function FastForwardButton({
   index,
   setIndex,
-  audioFile
+  audioFile,
+  randomIndices,
 }: {
   index: number;
   setIndex: Dispatch<SetStateAction<number>>;
   audioFile: HTMLAudioElement | null;
+  randomIndices: number[];
 }) {
+  const { shuffle } = usePlayer();
   const next = () => {
     // only if the audio file exists, increment the index
-    if (audioFile) setIndex(index + 1);
+    if (audioFile) {
+      // based on the shuffle variable
+      shuffle
+        ? setIndex(randomIndices[randomIndices.indexOf(index) + 1])
+        : setIndex(index + 1);
+    }
   };
   return (
     <button onClick={next}>
--- app/components/ControlButtons/RewindButton.tsx
+++ app/components/ControlButtons/RewindButton.tsx
@@ -1,3 +1,4 @@
+import { usePlayer } from '@/app/context/PlayerContext';
 import { Rewind } from 'lucide-react';
 import { Dispatch, SetStateAction } from 'react';
 
@@ -5,15 +6,29 @@ export default function RewindButton({
   index,
   setIndex,
   audioFile,
+  randomIndices,
 }: {
   index: number;
   setIndex: Dispatch<SetStateAction<number>>;
   audioFile: HTMLAudioElement | null;
+  randomIndices: number[];
 }) {
+  const { shuffle } = usePlayer();
   const previous = () => {
     if (audioFile) {
       // if the audioFile exists and the current time is less than 3 seconds, go back to previous song
-      if (audioFile.currentTime <= 3) setIndex(index > 0 ? index - 1 : 0); // check for negative index
+      if (audioFile.currentTime <= 3) {
+        // extra checks for negative index
+        const curIndex = shuffle ? randomIndices.indexOf(index) : index;
+        // if the index is not shuffled and zero, just set to zero
+        if (!shuffle && curIndex === 0) setIndex(0);
+        // if the index in the shuffled array is the 0th element, just set to zero
+        else if (shuffle && curIndex === 0) setIndex(0);
+        else {
+          // otherwise, set the index to the previous element
+          setIndex(shuffle ? randomIndices[curIndex - 1] : curIndex - 1);
+        }
+      }
       // if the audioFile exists and the current time is greater than 3 seconds, go back to the beginning of the song
       else audioFile.currentTime = 0;
     }
