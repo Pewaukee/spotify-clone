0fe2ff47108769247328dc57955cdd9f4d65b100
Karthik Shankar <misterkar6@gmail.com>
2023-07-24 21:28:41 -0700

    fast forward and rewind

--- app/components/ControlButtons/RewindButton.tsx
+++ app/components/ControlButtons/RewindButton.tsx
@@ -4,14 +4,20 @@ import { Dispatch, SetStateAction } from 'react';
 export default function RewindButton({
   index,
   setIndex,
+  audioFile,
 }: {
   index: number;
   setIndex: Dispatch<SetStateAction<number>>;
+  audioFile: HTMLAudioElement | null;
 }) {
   const previous = () => {
-    setIndex(index - 1);
+    if (audioFile) {
+      // if the audioFile exists and the current time is less than 3 seconds, go back to previous song
+      if (audioFile.currentTime <= 3) setIndex(index > 0 ? index - 1 : 0); // check for negative index
+      // if the audioFile exists and the current time is greater than 3 seconds, go back to the beginning of the song
+      else audioFile.currentTime = 0;
+    }
   };
-
   return (
     <button onClick={previous}>
       <Rewind
