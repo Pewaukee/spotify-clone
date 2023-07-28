599a36bbaf86ac3d5157b1443ce2621b6f3df901
Karthik Shankar <misterkar6@gmail.com>
2023-07-28 16:47:36 -0700

    fast forward repeat_one change

--- app/components/ControlButtons/FastForwardButton.tsx
+++ app/components/ControlButtons/FastForwardButton.tsx
@@ -1,5 +1,6 @@
-import { Song } from '@/app/context/PlayerContext';
+import { RepeatMode, Song, usePlayer } from '@/app/context/PlayerContext';
 import { FastForward } from 'lucide-react';
+import React from 'react';
 
 export default function FastForwardButton({
   setCurrentSong,
@@ -8,9 +9,22 @@ export default function FastForwardButton({
   setCurrentSong: (newCurrentSong: Song) => void;
   findNextSong: () => Song;
 }) {
+
+  // for repeat changes on skip
+  const { repeat, setRepeat } = usePlayer();
+
   const next = () => {
+    /**
+     * in spotify, the repeat mode is set
+     * back to repeat all if a current song
+     * is set to repeat one and skipped
+     */
+    if (repeat === RepeatMode.REPEAT_ONE) {
+      setRepeat(RepeatMode.REPEAT_ALL);
+    }
     setCurrentSong(findNextSong());
   };
+
   return (
     <button onClick={next}>
       <FastForward
