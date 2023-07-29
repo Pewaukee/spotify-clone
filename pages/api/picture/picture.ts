7c54e6af97ed20e5d28ba2c8437f0211a3107854
Karthik Shankar <misterkar6@gmail.com>
2023-07-29 15:04:21 -0700

    picture endpoint

---
+++ pages/api/picture/picture.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// get a random square picture from the unsplash api
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const response = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        orientation: 'squarish',
+        client_id: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY,
+        count: 1,
+        query: 'music album', // search for pictures related to music
+      },
+    });
+
+    const picture = response.data[0];
+
+    const src = picture.urls.small;
+
+    return res.json({
+      src,
+    });
+  }
+}
