ca9301e9fe6c59e13ced1d33992feedcb8f10bc7
Karthik Shankar <misterkar6@gmail.com>
2023-07-29 15:05:05 -0700

    spotify svg for error, also error files

--- app/playlist/error.tsx
+++ app/playlist/error.tsx
@@ -5,24 +5,26 @@ import Link from 'next/link';
 
 export default function Error({ error }: { error: Error }) {
   return (
-    <div className='bg-gray-200 px-9 py-14 shadow-lg rounded text-black'>
-      <h3 className='text-3xl font-bold'>Well, this is embarrassing</h3>
-      <p className='text-reg font-bold'>{error.message}</p>
-      <p className='mt-6 text-sm font-light'>Error Code: 400</p>
-      <div className='grid grid-cols-2'>
-        <div className='mt-6 justify-center items-center rounded-full bg-black inline-block bg-green-400 w-[50%]'>
-          <Link href='/'>
-            <div className='flex flex-row justify-center items-center'>
-              Home <Home className='pl-[5px]' />
-            </div>
-          </Link>
-        </div>
-        <div className='mt-6 justify-self-end items-center rounded-full bg-black inline-block bg-green-400 w-[60%]'>
-          <Link href='/Library'>
-            <div className='flex flex-row justify-center items-center'>
-              Library <FileAudio2 className='pl-[5px]' />
-            </div>
-          </Link>
+    <div className='h-screen bg-gray-700 flex flex-col justify-center items-center'>
+      <div className='bg-gray-200 px-9 py-14 shadow-lg rounded text-black'>
+        <h3 className='text-3xl font-bold'>Well, this is embarrassing</h3>
+        <p className='text-reg font-bold'>{error.message}</p>
+        <p className='mt-6 text-sm font-light'>Error Code: 400</p>
+        <div className='grid grid-cols-2'>
+          <div className='mt-6 justify-center items-center rounded-full bg-black inline-block bg-green-400 w-[50%]'>
+            <Link href='/'>
+              <div className='flex flex-row justify-center items-center'>
+                Home <Home className='pl-[5px]' />
+              </div>
+            </Link>
+          </div>
+          <div className='mt-6 justify-self-end items-center rounded-full bg-black inline-block bg-green-400 w-[60%]'>
+            <Link href='/Library'>
+              <div className='flex flex-row justify-center items-center'>
+                Library <FileAudio2 className='pl-[5px]' />
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
