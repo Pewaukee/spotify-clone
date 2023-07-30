277250fac5fd5fabf1cfaca8e89761bb26821c49
Karthik Shankar <misterkar6@gmail.com>
2023-07-30 16:20:12 -0700

    album data

---
+++ data/albums.ts
@@ -0,0 +1,74 @@
+export const homeAlbums = [
+    {
+        image: {
+            src: '/albumCovers/immunity.png',
+            alt: 'Immunity Clairo Album Cover',
+        },
+        title: 'Immunity',
+        artist: 'Clairo',
+    },
+    {
+        image: {
+            src: '/albumCovers/swimming.png',
+            alt: 'Swimming Mac Miller Album Cover',
+        },
+        title: 'Swimming',
+        artist: 'Mac Miller',
+    },
+    {
+        image: {
+            src: '/albumCovers/free_spirit.png',
+            alt: 'Free Spirit Khalid Album Cover',
+        },
+        title: 'Free Spirit',
+        artist: 'Khalid',
+    },
+    {
+        image: {
+            src: '/albumCovers/change_of_scenery.png',
+            alt: 'Change of Scenery Quinn XCII Album Cover',
+        },
+        title: 'Change of Scenery',
+        artist: 'Quinn XCII',
+    },
+    {
+        image: {
+            src: '/albumCovers/under_pressure.png',
+            alt: 'Under Pressure Logic Album Cover',
+        },
+        title: 'Under Pressure',
+        artist: 'Logic',
+    },
+    {
+        image: {
+            src: '/albumCovers/industry_plant.png',
+            alt: 'Industry Plant Iann Dior Album Cover',
+        },
+        title: 'Industry Plant',
+        artist: 'Iann Dior',
+    },
+    {
+        image: {
+            src: '/albumCovers/come_home_the_kids_miss_you.png',
+            alt: 'Come Home The Kids Miss You Jack Harlow Album Cover',
+        },
+        title: 'Come Home The Kids Miss You',
+        artist: 'Jack Harlow',
+    },
+    {
+        image: {
+            src: '/albumCovers/circles.jpg',
+            alt: 'Circles Mac Miller Album Cover',
+        },
+        title: 'Circles',
+        artist: 'Mac Miller',
+    },
+    {
+        image: {
+            src: '/albumCovers/1999.png',
+            alt: '1999 Rich Brian Album Cover',
+        },
+        title: '1999',
+        artist: 'Rich Brian',
+    }
+]
