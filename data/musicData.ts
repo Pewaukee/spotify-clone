33755b2277bfd3e4704c065e78905b7777d0d9fb
Karthik Shankar <misterkar6@gmail.com>
2023-07-29 18:24:47 -0700

    music data file

---
+++ data/musicData.ts
@@ -0,0 +1,13 @@
+// export the type according to the music data to be used among the app
+export type MusicData = {
+  albumCover: string;
+  tracks: {
+    title: string;
+    duration: number;
+    explicit_lyrics: boolean;
+    track_position: number;
+    preview: string;
+  }[];
+  artistName: string;
+  artistPicture: string;
+} | null;
