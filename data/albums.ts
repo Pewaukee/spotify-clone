3deaed75fc6df14767346c7ec179e901f0c36a97
Karthik Shankar <misterkar6@gmail.com>
2023-07-30 16:32:55 -0700

    adding type for music data

--- data/albums.ts
+++ data/albums.ts
@@ -1,74 +1,91 @@
-export const homeAlbums = [
-    {
-        image: {
-            src: '/albumCovers/immunity.png',
-            alt: 'Immunity Clairo Album Cover',
-        },
-        title: 'Immunity',
-        artist: 'Clairo',
+export const homeAlbums: {
+  image: {
+    src: string;
+    alt: string;
+  };
+  title: string;
+  artist: string;
+  type: 'Album' | 'Playlist';
+}[] = [
+  {
+    image: {
+      src: '/albumCovers/immunity.png',
+      alt: 'Immunity Clairo Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/swimming.png',
-            alt: 'Swimming Mac Miller Album Cover',
-        },
-        title: 'Swimming',
-        artist: 'Mac Miller',
+    title: 'Immunity',
+    artist: 'Clairo',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/swimming.png',
+      alt: 'Swimming Mac Miller Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/free_spirit.png',
-            alt: 'Free Spirit Khalid Album Cover',
-        },
-        title: 'Free Spirit',
-        artist: 'Khalid',
+    title: 'Swimming',
+    artist: 'Mac Miller',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/free_spirit.png',
+      alt: 'Free Spirit Khalid Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/change_of_scenery.png',
-            alt: 'Change of Scenery Quinn XCII Album Cover',
-        },
-        title: 'Change of Scenery',
-        artist: 'Quinn XCII',
+    title: 'Free Spirit',
+    artist: 'Khalid',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/change_of_scenery.png',
+      alt: 'Change of Scenery Quinn XCII Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/under_pressure.png',
-            alt: 'Under Pressure Logic Album Cover',
-        },
-        title: 'Under Pressure',
-        artist: 'Logic',
+    title: 'Change of Scenery',
+    artist: 'Quinn XCII',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/under_pressure.png',
+      alt: 'Under Pressure Logic Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/industry_plant.png',
-            alt: 'Industry Plant Iann Dior Album Cover',
-        },
-        title: 'Industry Plant',
-        artist: 'Iann Dior',
+    title: 'Under Pressure',
+    artist: 'Logic',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/industry_plant.png',
+      alt: 'Industry Plant Iann Dior Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/come_home_the_kids_miss_you.png',
-            alt: 'Come Home The Kids Miss You Jack Harlow Album Cover',
-        },
-        title: 'Come Home The Kids Miss You',
-        artist: 'Jack Harlow',
+    title: 'Industry Plant',
+    artist: 'Iann Dior',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/come_home_the_kids_miss_you.png',
+      alt: 'Come Home The Kids Miss You Jack Harlow Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/circles.jpg',
-            alt: 'Circles Mac Miller Album Cover',
-        },
-        title: 'Circles',
-        artist: 'Mac Miller',
+    title: 'Come Home The Kids Miss You',
+    artist: 'Jack Harlow',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/circles.jpg',
+      alt: 'Circles Mac Miller Album Cover',
     },
-    {
-        image: {
-            src: '/albumCovers/1999.png',
-            alt: '1999 Rich Brian Album Cover',
-        },
-        title: '1999',
-        artist: 'Rich Brian',
-    }
-]
+    title: 'Circles',
+    artist: 'Mac Miller',
+    type: 'Album',
+  },
+  {
+    image: {
+      src: '/albumCovers/1999.png',
+      alt: '1999 Rich Brian Album Cover',
+    },
+    title: '1999',
+    artist: 'Rich Brian',
+    type: 'Album',
+  },
+];
