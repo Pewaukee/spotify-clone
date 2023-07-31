1609a6f98f164f81c8215ca4f56a9f5c8fda96e2
Karthik Shankar <misterkar6@gmail.com>
2023-07-31 16:07:53 -0700

    mobile changes for volume, also mute button

--- app/components/VolumeSlider.tsx
+++ app/components/VolumeSlider.tsx
@@ -6,7 +6,6 @@ import React from 'react';
 import { usePlayer } from '../context/PlayerContext';
 
 export default function VolumeSlider() {
-
   const { volume, setVolume } = usePlayer();
 
   const renderVolumeIcon = () => {
@@ -20,21 +19,32 @@ export default function VolumeSlider() {
     return <Volume2 />;
   };
 
+  // mute or unmute the volume if the volume icon is clicked
+  const handleClick = () => {
+    console.log(volume[0])
+    if (volume[0] === 0) {
+      setVolume([20]);
+    } else {
+      setVolume([0]);
+    }
+  };
+
   return (
     <div className='flex flex-row justify-center items-center'>
-        <div className='mr-2'>
-        {renderVolumeIcon()}
-        </div>
-      
+      <div className='mr-[5px]'>
+        <button onClick={handleClick}>{renderVolumeIcon()}</button>
+      </div>
+
       <form>
         <Slider.Root
-          className='relative flex items-center select-none touch-none w-[100px] h-[20px] group/volume'
+          className='relative flex items-center select-none touch-none w-[80px] h-[20px] group/volume'
           defaultValue={[50]}
           max={100}
           step={1}
           onValueChange={(e) => setVolume(e)}
+          value={volume}
         >
-          <Slider.Track className='relative grow rounded-full h-[3px] bg-gray-400'>
+          <Slider.Track className='relative self-center grow rounded-full h-[3px] bg-gray-400'>
             <Slider.Range className='absolute bg-white rounded-full h-full group-hover/volume:bg-green-400' />
           </Slider.Track>
           <Slider.Thumb
