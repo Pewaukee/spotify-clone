336fd82c90c4279f9740a51bd797fde2097af891
Karthik Shankar <misterkar6@gmail.com>
2023-07-31 21:57:32 -0700

    setting up the song mapping

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Navbar from '../components/Navbar';
 
 export const metadata: Metadata = {
   title: 'Search - Spofity Clone',
@@ -11,7 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main>{children}</main>
-    
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
   );
 }
