acba9dab9839555f582388642ef186fb62179883
Karthik Shankar <51296787+Pewaukee@users.noreply.github.com>
2023-09-24 10:16:30 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className='w-full h-full bg-neutral-700'>
       <Navbar />
       <div className='flex justify-center items-center text-xl mt-4 ml-2 mr-2 md:mt-10 md:ml-10 md:text-2xl md:items-start md:justify-normal'>
-        <h1>Pewaukee&apos;s favorite tracks</h1>
+        <h1>Welcome to the Spotify Clone!</h1>
       </div>
       <div className='w-full'>
         <div className='m-2 pb-[200px] md:m-4 grid grid-cols-3 gap-4 md:grid-cols-5'>
